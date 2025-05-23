8a0b6b84d04fc455263ae1fe7dabc514203ae25d
SunsetFactory <yang1194us@gmail.com>
2025-05-23 21:08:49 +0900

    chore: 주석 날림

--- src/app/components/DeckBuilder/Molecules/CardGrid/CardGrid.js
+++ src/app/components/DeckBuilder/Molecules/CardGrid/CardGrid.js
@@ -3,22 +3,6 @@
 import "./CardGrid.css";
 import SelectableCard from "../SelectableCard";
 
-// export default function CardGrid({ cards, selectedIds = [], onSelect }) {
-//   return (
-//     <div className="card-grid">
-//       {cards.map((card) => (
-//         <SelectableCard
-//           key={card.id}
-//           id={card.id}
-//           image={card.image}
-//           selected={selectedIds.includes(card.id)}
-//           onSelect={onSelect}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
 export default function CardGrid({ cards, selectedCounts = {}, onSelect }) {
   return (
     <div className="card-grid">
